7757f289d04acaa79923f1569af547eea7e035c8
Stephen Wool <stevewool7@gmail.com>
2019-03-08 15:21:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list li')
+  
+  for  (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML =  parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var nodes = document.querySelectorAll('#grand-node div')
+  
+  for (let i = 0; i < nodes.length; i++) {
+    if(nodes[i].childElementCount === 0) {
+      return nodes[i];
+    }
+  }
+}
+
 
