475e70ef63a1e0aa7547896ee4cf27a56e17e7c7
Stephen Wool <stevewool7@gmail.com>
2019-03-08 14:57:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('.ranked-list')
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list li')
 }
